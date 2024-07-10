609249ed0960b4a62e4d9b7f4f12317b1b0d1f73
DenPro117 <wangpanpan227@gmail.com>
2024-07-11 03:09:38 +0800

    feat: add template, types, utils

---
+++ src/core/templates/desktop/app.tsx
@@ -0,0 +1,19 @@
+import ReactDom from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {configureStore} from '@core/store';
+import {AliveScope} from 'react-activation';
+import './styles/main.scss';
+
+export const store = configureStore();
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+export default (Routes: React.FC): void => {
+  root.render(
+      // @ts-ignore
+      <Provider store={store}>
+        <AliveScope>
+          <Routes />
+        </AliveScope>
+      </Provider>,
+  );
+};
---
+++ src/core/templates/desktop/components/Popup/index.tsx
@@ -0,0 +1,57 @@
+import Overlay from '../Overlay';
+import css from './popup.scss';
+
+interface IProps {
+  display: boolean
+  zIndex?: number
+  height?: string
+  width?: string
+  title: string
+  close?: Function
+  header?: React.ReactElement
+  children: any
+  sharp?: boolean
+  className?: string
+  top?: string
+}
+export default function({
+  display = false,
+  sharp = false,
+  zIndex = 100,
+  height = '',
+  width = '',
+  title,
+  close,
+  header,
+  children,
+  className,
+  top = '',
+}: IProps) {
+  if (!display) return (<></>);
+  return (
+    <Overlay zIndex={zIndex} display={display}>
+      <div className={css.wrapper}>
+        {close && <div className={css.cover} onClick={() => close()} />}
+        <div
+          className={`popup-wrapper ${sharp && 'sharp'}`}
+          style={{
+            height,
+            width,
+            top,
+            backgroundSize: '100%',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          {header || (
+            <div className="title">
+              {close && <span onClick={() => close()}>X</span>}
+              {title}
+              <div className="line" />
+            </div>
+          )}
+          <div className={`wrapper ${className}`}>{children}</div>
+        </div>
+      </div>
+    </Overlay>
+  );
+}
